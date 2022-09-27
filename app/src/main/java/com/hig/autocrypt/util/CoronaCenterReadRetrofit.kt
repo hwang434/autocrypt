3b43b146127defba2f52ebbf9bcff8befa826325
hwang434 <inqbator@knou.ac.kr>
2022-09-27 20:13:54 +0900

    Refactoring Useless code.

--- app/src/main/java/com/hig/autocrypt/util/CoronaCenterReadRetrofit.kt
+++ app/src/main/java/com/hig/autocrypt/util/CoronaCenterReadRetrofit.kt
@@ -1,6 +1,5 @@
 package com.hig.autocrypt.util
 
-import com.google.gson.GsonBuilder
 import com.hig.autocrypt.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,8 +8,7 @@ object CoronaCenterReadRetrofit {
     private const val BASE_URL = BuildConfig.CORONA_BASE_URL
 
     private fun getCoronaCenterReadRetrofit(): Retrofit {
-        val gson = GsonBuilder().create()
-        return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build()
+        return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
     }
 
     fun getCoronaRequest(): CoronaRequest {
