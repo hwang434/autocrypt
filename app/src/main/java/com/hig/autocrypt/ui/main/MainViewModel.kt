b3856f4b51c091850b608ee1735bdde2ce18eb95
hwang434 <inqbator@knou.ac.kr>
2022-09-28 10:47:23 +0900

    Percentage Setting is done. When app is created, start animation for percentage until 80%. when database operation is done. start animation for percentage 80% to 100%.

--- app/src/main/java/com/hig/autocrypt/ui/main/MainViewModel.kt
+++ app/src/main/java/com/hig/autocrypt/ui/main/MainViewModel.kt
@@ -1,7 +1,54 @@
 package com.hig.autocrypt.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    companion object {
+        private const val TAG: String = "로그"
+    }
+
+    private val _downloadPercentage = MutableStateFlow<Int>(0)
+    val downloadPercentage = _downloadPercentage
+
+    private val _isResponseEnd = MutableStateFlow<String>("hi")
+    val isResponseEnd = _isResponseEnd
+
+    private val _isInsertedToDatabase = MutableStateFlow<Boolean>(false)
+    val isInsertedToDatabase = _isInsertedToDatabase
+
+    fun makeInsertedToDatabaseEnd() {
+        Log.d(TAG,"MainViewModel - makeInsertedToDatabaseEnd() called")
+        viewModelScope.launch(Dispatchers.IO) {
+            delay(3000)
+            _isInsertedToDatabase.emit(true)
+        }
+    }
+
+    fun makePercentageEighty() {
+        Log.d(TAG,"MainViewModel - makePercentageEighty() called")
+        viewModelScope.launch(Dispatchers.IO) {
+            // Multiply i with 5%. start percentage is 5%. end percentage is 80%.
+            for (i in 1..16) {
+                delay(100)
+                _downloadPercentage.emit(5 * i)
+            }
+        }
+    }
+
+    fun makePercentageEightyToHundred() {
+        Log.d(TAG,"MainViewModel - makePercentageEightyToHundred() called")
+        viewModelScope.launch(Dispatchers.IO) {
+            // Multiply i with 5%. start percentage is 85%. end percentage is 100%.
+            for (i in 17..20) {
+                delay(100)
+                _downloadPercentage.emit(5 * i)
+            }
+        }
+    }
 }
