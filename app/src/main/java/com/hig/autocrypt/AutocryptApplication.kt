83f5f5261e30b38f64ce9227fd4ef15b2b91ce64
hwang434 <inqbator@knou.ac.kr>
2022-09-28 23:30:57 +0900

    네이버맵 api 설정 완료. 네이버 맵 API 설정을 완료했다. 공식 API 문서에 따르면 build.gradle(루트)에 저장소를 추가하라고 나와있는데, 해당 경로에 allprojects가 없어서 검색해봤더니 settings.gradle에 설정하라는 말이 많았다. 그래서 settings.gradle에 설정했는데, 오류가 나서 거의 2시간 정도 헤멨다. 그나마 다행인 게 네이버 측에서 공식 레포지토리를 제공해서 해당 레포지토리를 참고해서 문제를 해결했다.

--- app/src/main/java/com/hig/autocrypt/AutocryptApplication.kt
+++ app/src/main/java/com/hig/autocrypt/AutocryptApplication.kt
@@ -1,8 +1,20 @@
 package com.hig.autocrypt
 
 import android.app.Application
+import android.util.Log
+import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AutocryptApplication: Application() {
+    companion object {
+        private const val TAG = "로그"
+    }
+    override fun onCreate() {
+        Log.d(TAG, "AutocryptApplication - onCreate()")
+        super.onCreate()
+        NaverMapSdk.getInstance(this).setClient(
+            NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_MAP_CLIENT_ID)
+        )
+    }
 }
