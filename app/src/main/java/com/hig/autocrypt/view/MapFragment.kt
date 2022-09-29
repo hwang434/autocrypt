7ed275e8b08935db1ac5655658dc2a9a2272b4a4
hwang434 <inqbator@knou.ac.kr>
2022-09-29 17:34:50 +0900

    Reduce the size of Marker Marker is too big. I cann't distinct marks. So reduce the size of marker to 80w*100h

--- app/src/main/java/com/hig/autocrypt/view/MapFragment.kt
+++ app/src/main/java/com/hig/autocrypt/view/MapFragment.kt
@@ -180,6 +180,8 @@ class MapFragment : Fragment() {
                     marker.position = LatLng(publicHealth.lat, publicHealth.lng)
                     marker.map = naverMap
                     marker.captionText = publicHealth.facilityName
+                    marker.width = 80
+                    marker.height = 100
 
                     when (publicHealth.centerType) {
                         "중앙/권역" -> {
