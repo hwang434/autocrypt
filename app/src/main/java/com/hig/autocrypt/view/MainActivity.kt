39ecf7a6b2ff15850e638e99c7cb76d5051e2fe0
hwang434 <inqbator@knou.ac.kr>
2022-10-10 23:36:51 +0900

    Change method of exit application & Refactoring change finish() to finishAndRemoveTask() for completely finish application. Add lifecycle logging for MainActivity.kt. Add onCleared logging for MainViewModel.kt

--- app/src/main/java/com/hig/autocrypt/view/MainActivity.kt
+++ app/src/main/java/com/hig/autocrypt/view/MainActivity.kt
@@ -16,10 +16,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityMainBinding
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG,"MainActivity - onCreate() called")
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.d(TAG,"MainActivity - onStart() called")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG,"MainActivity - onResume() called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"MainActivity - onPause() called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"MainActivity - onStop() called")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"MainActivity - onDestroy() called")
+        super.onDestroy()
     }
 }
